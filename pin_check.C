091394a2832b397b7b6c43f4b7e0df9d2c00ad91
shivanshu bajpai <shivanshubajpai1086@gmail.com>
2018-09-04 23:32:02 +0530

    Enter the pin "abcd",it will check with some delay..

---
+++ pin_check.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include <stdlib.h>
+#include <dos.h>
+#include <time.h>
+#include <windows.h>
+#include <unistd.h>
+
+main()
+{
+ int i,b;
+ char chr[5];
+ printf("Enter the pin number of your ATM\n");
+ for(i=0;i<4;i++)
+ {
+    chr[i]=getch();
+    printf("*");
+ }
+ chr[4]='\0';
+ b=strcmp("abcd",chr);
+ if(b==0)
+ {
+    printf("Checking the pin......\n");
+    Sleep(3000);
+    printf("Congo!! your pin is correct\n");
+ }
+ else
+ {
+    printf("Checking the pin......\n");
+    Sleep(3000);
+    printf("oh oh...you have entered wrong pin");
+ }
+ return 0;
+ }
